2242c8629623e4632b470337e526c4fb338d2f4d
Henrik <Henrikmattsson89@gmail.com>
2025-01-14 07:08:11 +0100

    added sqlite3, created js to create DB

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# CLI-tool
+## CLI-tool
+
+#steps
+-npm init -y
+-npm install sqlite3
+-create spliteDB.js
+-to create test database
---
+++ sqliteDB.js
@@ -0,0 +1,31 @@
+import sqlite3 from "sqlite3";
+sqlite3.verbose();
+
+// open database
+const db = new splite3.Database("mydatabase.db");
+
+// create table
+
+db.serialize(() => {
+	db.run(
+		"CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT NOT NULL, category TEXT, price REAL"
+	);
+
+	// insert data
+	const stmt = db.prepare(
+		"INSERT INTO items (name,category,price) VALUES (?,?,?)"
+	);
+	stmt.run("Apple", "Fruit", 0.5);
+	stmt.run("Banana", "Fruit", 0.5);
+	stmt.run("Carrot", "Vegetable", 0.4);
+	stmt.finalize();
+
+	// query data
+	db.each("SELECT id, name, category, price FROM items", (err, row) => {
+		if (err) console.error(err.message);
+		else console.log(row);
+	});
+});
+
+// close the database
+db.close();
